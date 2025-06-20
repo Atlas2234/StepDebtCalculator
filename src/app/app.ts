ec829741f999ab778bf0ffe929d08d071c85d2a8
Atlas2234 <dlz988675@gmail.com>
2025-06-19 20:11:07 -0400

    added styling for the SPA

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserInput } from "./user-input/user-input";
 import { Output } from "./output/output";
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserInput, Output],
+  imports: [RouterOutlet, UserInput, Output, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
